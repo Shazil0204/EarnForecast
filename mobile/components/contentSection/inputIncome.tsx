9d3ef141d8892fee7440b25c2c91e20683a06d9d
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-10-31 09:20:50 +0100

    extra components

---
+++ mobile/components/contentSection/inputIncome.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const InputIncome = () => {
+  return (
+    <View>
+      <Text>InputIncome</Text>
+    </View>
+  );
+};
+
+export default InputIncome;
+
+const styles = StyleSheet.create({});

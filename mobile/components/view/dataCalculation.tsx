dc08ca9f0be139754bcc45ec4ccda0b4153991b0
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-11-01 12:24:51 +0100

    Create dataCalculation.tsx

---
+++ mobile/components/view/dataCalculation.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DataCalculation = () => {
+  console.log("dataCalc");
+  return (
+    <View style={{ backgroundColor: "#fff", padding: 100 }}>
+      <Text>DataCalculation</Text>
+    </View>
+  );
+};
+
+export default DataCalculation;

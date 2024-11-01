0f3d355be6318049ccce364d10f98f0dedd96523
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-11-01 12:29:51 +0100

    Update dataCalculation.tsx

--- mobile/components/view/dataCalculation.tsx
+++ mobile/components/view/dataCalculation.tsx
@@ -1,7 +1,23 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const DataCalculation = () => {
+interface DataCalculationProp {
+  startTime: Date | null;
+  endTime: Date | null;
+  date: Date | null;
+  km: number | null;
+  income: number | null;
+  tip: number | null;
+}
+
+const DataCalculation: React.FC<DataCalculationProp> = ({
+  startTime,
+  endTime,
+  date,
+  km,
+  income,
+  tip,
+}) => {
   console.log("dataCalc");
   return (
     <View style={{ backgroundColor: "#fff", padding: 100 }}>
